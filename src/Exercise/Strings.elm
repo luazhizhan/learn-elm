b4e9693d52eb09824b60acba3e9e64636833d626
Steven Yap <stevenyap@futureworkz.com>
2020-08-25 12:01:48 +0800

    Add Strings exercise

---
+++ src/Exercise/Strings.elm
@@ -0,0 +1,12 @@
+module Exercise.Strings exposing (format)
+
+{-| Write a function that formats a string from
+"elm/ is/fun /to/write!" to "ELM IS FUN TO WRITE!"
+Hint: You need to split the string, trim out the white spaces, join them back and uppercase it.
+-}
+
+
+format : String -> String
+format string =
+    -- Your implementation here
+    string
---
+++ tests/Spec/Exercise/Strings.elm
@@ -0,0 +1,16 @@
+module Spec.Exercise.Strings exposing (suite)
+
+import Exercise.Strings exposing (format)
+import Expect
+import Test exposing (Test, describe, test)
+
+
+suite : Test
+suite =
+    Test.skip <|
+        describe "format"
+            [ test "Formats \"elm/ is/fun /to/write!\"" <|
+                \_ -> Expect.equal "ELM IS FUN TO WRITE!" (format "elm/ is/fun /to/write!")
+            , test "Formats \"hello/world\"" <|
+                \_ -> Expect.equal "HELLO WORLD" (format "hello/world")
+            ]
